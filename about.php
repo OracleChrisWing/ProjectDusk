c0aff2b92fe6143a2e64a119e6099b60876a4505
OracleChrisWing <30342489+OracleChrisWing@users.noreply.github.com>
2018-01-21 20:27:46 +0000

    Initial commit

---
+++ about.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+<!-- Website template by freewebsitetemplates.com -->
+<html>
+<head>
+	<meta charset="UTF-8">
+	<?php include("includes/title.php"); ?>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+	<div id="header">
+		<div class="clearfix">
+			<?php include("includes/navigation.php"); ?>
+		</div>
+	</div>
+	<div id="contents">
+		<div class="clearfix">
+			<h1>About Us</h1>
+			<div class="frame2">
+				<div class="box">
+					<img src="images/thumb-up.jpg" alt="Img" height="298" width="924">
+				</div>
+			</div>
+			<h2>What we do</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+				Proin tristique turpis dui, vel tempor neque egestas in. 
+				Sed egestas tortor vel lectus pellentesque bibendum. 
+				Suspendisse enim erat, accumsan id orci quis, vestibulum rutrum elit. 
+				Ut non lacus tristique, fringilla purus ut, malesuada felis. 
+				Suspendisse mi orci, porta eget urna vitae, viverra eleifend risus. 
+				Proin feugiat dapibus magna a dictum. Duis eleifend feugiat elit sed molestie. 
+				Sed sit amet consectetur lorem. Morbi sed sagittis nisl. 
+				Sed venenatis tincidunt lorem, eget tincidunt nisi scelerisque at. 
+				Sed finibus urna convallis condimentum consectetur. 
+				Fusce eu pretium ipsum, nec varius nisi.
+			</p>
+			<h2>What we can offer you</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+				Proin tristique turpis dui, vel tempor neque egestas in. 
+				Sed egestas tortor vel lectus pellentesque bibendum. 
+				Suspendisse enim erat, accumsan id orci quis, vestibulum rutrum elit. 
+				Ut non lacus tristique, fringilla purus ut, malesuada felis. 
+				Suspendisse mi orci, porta eget urna vitae, viverra eleifend risus. 
+				Proin feugiat dapibus magna a dictum. Duis eleifend feugiat elit sed molestie. 
+				Sed sit amet consectetur lorem. Morbi sed sagittis nisl. 
+				Sed venenatis tincidunt lorem, eget tincidunt nisi scelerisque at. 
+				Sed finibus urna convallis condimentum consectetur. 
+				Fusce eu pretium ipsum, nec varius nisi.
+			</p>
+		</div>
+	</div>
+	<?php include("includes/footer.php"); ?>
+</body>
+</html>
